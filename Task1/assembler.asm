06bdc701bf642a1ae743b91e671c11801027aa88
Shwerch <ilia.popov06@gmail.com>
2025-03-30 19:01:54 +0700

    Assembler input convert to number now working

--- Task1/assembler.asm
+++ Task1/assembler.asm
@@ -1,80 +1,75 @@
+INPUT_SIZE=2
+CHAR_INT_DIFFERENCE=0x30
+
+STDIN=0
+STDOUT=1
+STDERR=2
+
+sys_write=1
+sys_read=0
+sys_exit=60
+
 .data
-msg:
+message:
   .ascii "Enter the N number: "
-  .set msg_len, . - msg
-inputSize:
-  .quad 8
+  .set message_len, . - message
 
 .text
 .globl _start
 
-output:
-  movq $1, %rax       # Номер функции sys_write
-  movq $1, %rdi       # Дескриптор стандартного выходного потока STDOUT
-  movq $msg, %rsi     # Адрес начала строки
-  movq $msg_len, %rdx # Длина строки
-  syscall
-  ret
+oneDigit:
+  subb $CHAR_INT_DIFFERENCE, %r8b
+  movb %r8b, %r10b
+  jmp digitFunctionEnd
 
-input:
-  movq $0, %rax         # Номер функции sys_read
-  movq $1, %rdi         # Дескриптор стандартного выходного потока STDOUT
-  movq %rsp, %rsi       # Адрес сохранения строки
-  movq $inputSize, %rdx # Длина строки
-  syscall
-  ret
-
-# %rsp - адрес начала строки
-# %rsi - адрес конца строки
-# %rbx - основание системы счисления
-# %r8 - адрес возврата
-toNumber:
-  movq $1, %rcx       # Множитель 10 в степени N
-  movq $0, %rdi       # Регистр для хранения результата
-  toNumberLoop:
-    movq $0, %rax     # Обнуляем %rax
-    movb 0(%rsi), %al # Сохранить код символа в %al
-    subq $48, %rax    # Преобразовать код цифры в цифру
-    mul %rcx          # Умножить на 10 в степени N
-    addq %rax, %rdi   # Добавить к результату в %rdi
-    subq $1, %rsi     # Сместь %rsi левее
-    cmpq %rsp, %rsi
-    jb toNumberLoopEnd
-    jmp toNumberLoop
-  toNumberLoopEnd:
-  jmp *%rdi
-  ret
+twoDigits:
+  subb $CHAR_INT_DIFFERENCE, %r8b
+  subb $CHAR_INT_DIFFERENCE, %r9b
+  movb %r9b, %r10b
+  movb %r8b, %al
+  movb $10, %r12b
+  mulb %r12b
+  addw %ax, %r10w
+  jmp digitFunctionEnd
 
 _start:
-  movq %rsp, %rbp # Сохранение начального состояния %rsp в %rbp
+  # Сохранение начального состояния %rsp в %rbp
+  movq %rsp, %rbp
   
   # Вывод приветственного сообщения
-  call output
-
-  subq $inputSize, %rsp # Выделение $inputSize байт в стеке
-  
-  # Ввод числа как массива символов
-  call input
-
-  r1:
-
-  # Преобразование строки в число
-  movq %rsp, %rsi         # Адрес начала строки
-  addq %rax, %rsi         # Прибавление кол-ва введенных символов к адресу начала строки
-  subq $2, %rsi           # Теперь %rsi содержит адрес последнего символа числа
-  movq $10, %rbx          # Основание системы счисления
-  movq $toNumberEnd, %r8  # Адрес возврата
-  jmp toNumber
-  toNumberEnd:
-  
-  movq %rbp, %rsp # Восстановление %rsp (освобождение стека)
-  movq $60, %rax  # Номер функции sys_exit
-  movq $0, %rdi   # Код возврата
+  movq $sys_write, %rax
+  movq $STDOUT, %rdi
+  movq $message, %rsi
+  movq $message_len, %rdx
   syscall
 
+  # Выделение INPUT_SIZE байт в стеке
+  subq $INPUT_SIZE, %rsp
   
-  # movq $1, %rax         # Номер функции sys_write
-  # movq $1, %rdi         # Дескриптор стандартного выходного потока STDOUT
-  # movq %rsp, %rsi       # Адрес начала строки
-  # movq $inputSize, %rdx # Длина строки
-  # syscall
+  # Ввод числа как массива INPUT_SIZE символов в %rsp
+  movq $sys_read, %rax
+  movq $STDIN, %rdi
+  movq %rsp, %rsi
+  movq $INPUT_SIZE, %rdx
+  syscall
+
+  # Перемещение символов в разные регистры
+  movb 0(%rsp), %r8b
+  movb 1(%rsp), %r9b
+  
+  # Определение, сколько цифр были введено и запись результата в %r10
+  cmpb $10, %r9b
+  je oneDigit
+  jmp twoDigits
+  digitFunctionEnd:
+
+  jmp exit
+
+exit:
+  # Восстановление %rsp (освобождение стека)
+  movq %rbp, %rsp
+
+  # Завершение рабоыт программы
+  movq $sys_exit, %rax
+  movq $0, %rdi
+  syscall
