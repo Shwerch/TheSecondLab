c9ba072897986e98420cc6b37df48d2a8c7bf79d
Shwerch <ilia.popov06@gmail.com>
2025-03-30 12:52:09 +0700

    Assembler

--- Task1/assembler.asm
+++ Task1/assembler.asm
@@ -2,78 +2,45 @@
 msg:
   .ascii "Enter the N number: "
   .set msg_len, . - msg
-system:
-  .ascii "'"
-  .set system_len, . - system
-first:
-  .quad 0
-second:
-  .quad 0
 
 .text
 .globl _start
 
-notice:
-  movq $1, %rax
-  movq $msg, %rsi
-  movq $msg_len, %rdx
+output:
+  movq $1, %rax       # Номер функции sys_write
+  movq $1, %rdi       # Дескриптор стандартного выходного потока
+  movq $msg, %rsi     # Адрес начала строки
+  movq $msg_len, %rdx # Длина строки
   syscall
   ret
 
 input:
-  movq $0, %rax
-  movq $0, %rdi
-  movq $first, %rsi
-  movq $1, %rdx
+  movq $0, %rax   # Номер функции sys_read
+  movq $0, %rdi   # Дескриптор стандартного входного потока
+  movq (%rsp), %rsi # Адрес сохранения строки
+  movq $8, %rdx   # Длина строки
   syscall
-  
-  movq $0, %rax
-  movq $0, %rdi
-  movq $second, %rsi
-  movq $1, %rdx
-  syscall
-
-  movq $second, %r8
-  movq $10, %r9
-  cmpq %r8, %r9
-  je exit
-
   ret
 
-output:
-  movq $1, %rax
-  movq $1, %rdi
-  movq $system, %rsi
-  movq $system_len, %rdx
-  syscall
-
-  movq $1, %rax
-  movq $1, %rdi
-  movq $first, %rsi
-  movq $2, %rdx
-  syscall
-
-  movq $1, %rax
-  movq $1, %rdi
-  movq $second, %rsi
-  movq $2, %rdx
-  syscall
-  
-  movq $1, %rax
-  movq $1, %rdi
-  movq $system, %rsi
-  movq $system_len, %rdx
+print:
+  movq $1, %rax       # Номер функции sys_write
+  movq $1, %rdi       # Дескриптор стандартного выходного потока
+  movq (%rsp), %rsi     # Адрес начала строки
+  movq $8, %rdx # Длина строки
   syscall
   ret
 
 _start:
-  movq $1, %rdi
-  call notice
-  call input
+  movq %rsp, %rbp # Сохранение начального состояния %rsp в %rbp
   call output
+  subq $8, %rsp   # Выделение 8 байт в стеке
+  call input
+  call print
   jmp exit
 
 exit:
-  movq $60, %rax
-  movq $0, %rdi
+  movq %rbp, %rsp # Восстановление %rsp (освобождение стека)
+  movq $60, %rax  # Номер функции sys_exit
+  movq $0, %rdi   # Код возврата
   syscall
+  ret
