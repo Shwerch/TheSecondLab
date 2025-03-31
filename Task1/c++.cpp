18677d4ddf3a277b58b7a11baaca48e9ffd40841
Shwerch <ilia.popov06@gmail.com>
2025-03-31 12:20:52 +0700

    C++

--- Task1/c++.cpp
+++ Task1/c++.cpp
@@ -4,6 +4,10 @@ int main() {
     int num = 0;
     std::cout << "Enter the N number: ";
     std::cin >> num;
+    if (num < 1 || num > 26) {
+        std::cout << "The N number must be from 1 to 26!" << std::endl;
+        return 1;
+    }
     for (int i = 0; i < num; i++) {
         for (int j = 0; j < i + 1; j++)
             std::cout << static_cast<char>(num - j - 1 + static_cast<int>('A')) << ' ';
