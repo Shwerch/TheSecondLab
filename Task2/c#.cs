cbc598cff74f566e047f86f4e56477d2f4d280f0
Shwerch <ilia.popov06@gmail.com>
2025-03-31 13:46:58 +0700

    C++ & C#

--- Task2/c#.cs
+++ Task2/c#.cs
@@ -22,6 +22,14 @@ class Program
         for (int i = 0; i < separators.Count - 1; i++)
         {
             count += (separators[i + 1] - separators[i] - 1) % 2;
+            for (int j = separators[i] + 1; j < separators[i + 1]; j++)
+            {
+                if (input[j] < '0' || input[j] > '9')
+                {
+                    Console.WriteLine("You should have entered the numbers!");
+                    return;
+                }
+            }
         }
         
         Console.WriteLine(count);
--- Task2/c++.cpp
+++ Task2/c++.cpp
@@ -16,11 +16,12 @@ int main() {
     int count = 0;
     for (int i = 0; i < separators.size() - 1; i++) {
         count += (separators[i + 1] - separators[i] - 1) % 2;
-        for (int j = separators[i] + 1; j < separators[i + 1]; j++)
+        for (int j = separators[i] + 1; j < separators[i + 1]; j++) {
             if (input[j] < '0' || input[j] > '9') {
                 std::cout << "You should have entered the numbers!" << std::endl;
                 return 1;
             }
+        }
     }
     std::cout << count << std::endl;
     return 0;
