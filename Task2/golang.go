bb2718f12dbb0350a1cb1b7dd6785d2a6e1b75a1
Shwerch <ilia.popov06@gmail.com>
2025-03-31 14:39:19 +0700

    Golang

---
+++ Task2/golang.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+ "fmt"
+)
+
+func main() {
+ fmt.Print("Enter the string of numbers with ',' separator: ")
+ var input string
+ fmt.Scanln(&input)
+
+ separators := []int{-1}
+ for i, char := range input {
+  if char == ',' {
+   separators = append(separators, i)
+  }
+ }
+ separators = append(separators, len(input))
+
+ count := 0
+ for i := 0; i < len(separators)-1; i++ {
+  count += (separators[i+1] - separators[i] - 1) % 2
+  for j := separators[i] + 1; j < separators[i+1]; j++ {
+   if input[j] < '0' || input[j] > '9' {
+    fmt.Println("You should have entered the numbers!")
+    return
+   }
+  }
+ }
+ fmt.Println(count)
+}
---
+++ Task3/golang.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+ "fmt"
+  "bufio"
+  "os"
+)
+
+func main() {
+ fmt.Print("Enter the string of numbers with ' ' separator: ")
+ input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+ separators := []int{-1}
+ for i, char := range input {
+  if char == ' ' {
+   separators = append(separators, i)
+  }
+ }
+ separators = append(separators, len(input))
+
+ numbers := make([][]rune, len(separators)-1)
+ for i := 0; i < len(separators)-1; i++ {
+  j := i + 1
+  numbers[i] = []rune{}
+  for k := separators[i] + 1; k < separators[j]; k++ {
+   numbers[i] = append(numbers[i], rune(input[k]))
+  }
+ }
+
+ answer := []int{}
+ for i := 0; i < len(numbers); i++ {
+  summ := 0
+  multi := 1
+  for _, j := range numbers[i] {
+   num := int(j - '0')
+   if num < 0 || num > 9 {
+    fmt.Println("You should have entered the numbers!")
+    return
+   }
+   summ += num
+   multi *= num
+  }
+  if summ < multi {
+   answer = append(answer, i)
+  }
+ }
+ for _, i := range answer {
+  fmt.Print(i, " ")
+ }
+ fmt.Println()
+}
