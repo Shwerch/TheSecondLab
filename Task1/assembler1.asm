bd909ebea42174b15f6c403af905f92e2c09a4d1
Shwerch <ilia.popov06@gmail.com>
2025-04-02 23:47:42 +0700

    Assembler non-final changes

--- Task1/assembler1.asm
+++ Task1/assembler1.asm
@@ -41,13 +41,15 @@ _start:
   pushq %r11
   pushq %r12
   pushq %r13
+  pushq %r14
+  pushq %r15
 
   movq $0, %r8
   movq $0, %r9
 
   movq %rsp, %r10
   addq $INPUT, %r10
-  addq $64, %r10  # Так как было сохранено 8 регистров 8 байт
+  addq $80, %r10  # Так как было сохранено 10 регистров 8 байт
   
   movb 0(%r10), %r8b
   movb 1(%r10), %r9b
@@ -97,22 +99,55 @@ _start:
   movq $0, %r9
   movq $0, %r10
 
-  cmpb $1, %r8
+  cmpq $1, %r8
   jb error
-  cmpb $26, %r8
+  cmpq $26, %r8
   ja error
 
   movq %r8, %rax
-  addq $1, %rax
+  addq $2, %rax
   movq %r8, %rdx
   mulq %rdx
   movq %rax, %r9
 
+  movq %rsp, %r15
   subq %r9, %rsp
 
+  movq %r8, %r10
+  movq 0, %r11
+  firstLoop:
 
+    movq %r11, %r12
+    addq $1, %r12
+    movq 0, %r13
+    secondLoop:
+
+      movq %r8, %r14
+      subq %r13, %r14
+      subq $1, %r14
+
+      movb %r14b, 0(%r15)
+      movb $' ', 1(%r15)
+      addq $2, %r15
+
+      addq $1, %r13
+      cmpq %r12, %r13
+      je secondLoopEnd
+      jmp secondLoop
+    secondLoopEnd:
+
+    movb $'\n', 0(%r15)
+    addq $1, %r15
+
+    addq $1, %r11
+    cmpq %r10, %r11
+    je firstLoopEnd
+    jmp firstLoop
+  firstLoopEnd:
 
   r1:
+  popq %r15
+  popq %r14
   popq %r13
   popq %r12
   popq %r11
